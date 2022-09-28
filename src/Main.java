cb2586f11c59f384b5b685253736c3090692aac2
alberto <alberto@mendezcabrera.com>
2022-09-28 02:14:57 +0200

    Añadidos los mensajes para indicar al usuario el estado de cada animal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import model.Donkey;
+import model.Horse;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Donkey donkey = new Donkey("Burrito Pacheco");
+        Horse horse = new Horse();
+        System.out.println("Nuestro burro se llama " + donkey.getName());
+        System.out.println("Tambien tenemos un caballo");
+        System.out.println("Tiene cola? " + horse.hasTail());
+        if(!horse.hasCrin()){
+            System.out.println("Tiene crin!!!");
+        }else{
+            System.out.println("No tiene crin!!!");
+        }
     }
 }
